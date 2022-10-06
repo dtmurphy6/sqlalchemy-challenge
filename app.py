fc7de09482f94fde65a7b71e0f07dc2c79b105c2
dtmurphy6 <102379855+dtmurphy6@users.noreply.github.com>
2022-10-06 10:48:35 -0500

    completed hw

--- app.py
+++ app.py
@@ -1,49 +1,158 @@
-from flask import Flask, jsonify
+#dependencies
 import numpy as np
+
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+from datetime import datetime
+import datetime as dt
+from flask import Flask, jsonify, Response
 
-app = Flask(__name__)
-
+# Database Setup
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
+
 # reflect the tables
 Base.prepare(engine, reflect=True)
 
+# Save reference to the table
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
-@app.route('/')
-def welcome():
-    return "Available routes:"
-    
-@app.route('/api/v1.0/precipitation')
+# Flask Setup
+app = Flask(__name__)
+
+# Flask Routes
+
+@app.route("/")
+def homepage():
+    """List all available routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end"
+    )
+
+@app.route("/api/v1.0/precipitation")
 def precipitation():
-    """Return a list of all passenger names, ordered by passenger count
-    """
+#   Convert the query results to a dictionary using date as the key and prcp as the value.
+    
+    # Create our session (link) from Python to the DB
     session = Session(engine)
+    results = session.query(Measurement.date, Measurement.prcp).order_by(Measurement.date).all()
+    session.close()
 
-    # Query all passengers
-    results = session.query(Measurement.date, Measurement.prcp).all()
+    # Create a dictionary from the row data and append to a list of temp_data
+    precip_data = []
+    for date, prcp in results: 
+        precip_dict = {}
+        #trying to see if I can return date as a dt object rather than a string? date_dt_object =  dt.datetime.strptime(date, "%Y-%m-%d")
+        precip_dict["date"] = date
+        precip_dict["prcp"] = prcp
+        precip_data.append(precip_dict)
 
-    # Convert list of tuples into normal list
-    prcp_data = list(np.ravel(results))
+#   Return the JSON representation of your dictionary.       
+    return jsonify(precip_data)
 
-    return jsonify(prcp_data)
-
-@app.route('/api/v1.0/stations')
+@app.route("/api/v1.0/stations")
 def stations():
-    """Return a list of all passenger names, ordered by passenger count
-    """
+#   Return a JSON list of stations from the dataset.
+    
     session = Session(engine)
-    # Query all passengers
     results = session.query(Station.station).all()
+    session.close()
 
-    # Convert list of tuples into normal list
-    station_data = list(np.ravel(results))
+    # stations_list = []
+    # for id, station in results:
+    #     station_dict = {}
+    #     station_dict["id"] = id
+    #     station_dict["station"] = station
+    #     stations_list.append(station_dict)
+    
+    station_list = list(np.ravel(results))
 
-    return jsonify(station_data)
+#   Return JSON list of stations
+    return jsonify(station_list)
+
+@app.route("/api/v1.0/tobs")
+def temperature():
+#   Query the dates and temperature observations of the most active station for the previous year of data.
+## Learning Assistant said to first find most active station in the entire data set, then find corresponding data   
+
+    session = Session(engine)
+    
+    #find station with highest count of measurements 
+    most_active_station_row = session.query(Station.station).filter(Station.station == Measurement.station).group_by(Station.station).order_by(func.count(Measurement.station).desc()).first()
+    most_active_station = most_active_station_row[0]
+    #find most recent date in measurement table
+    recent_date_string = session.query(Measurement.date).order_by(Measurement.date.desc()).first().date
+    #convert date string to datetime object
+    recent_date = datetime.strptime(recent_date_string, "%Y-%m-%d")
+    #calculate date to use in query for past year
+    query_date =  recent_date - dt.timedelta(days=365)
+    
+    #query for dates and temperatures, using filter for: most active station, date of measurement being >= query_date -- order results by date in ascending order
+    results = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date >= query_date).filter(Measurement.station == most_active_station).order_by(Measurement.date).all()
+    #close session
+    session.close()
+
+    #create list of temperatures
+    temp_results = [result.tobs for result in results]
+    temp_list = list(np.ravel(temp_results))
+
+#   Return a JSON list of temperature observations (TOBS) for the previous year.
+    return jsonify(temp_list)
+
+@app.route("/api/v1.0/<start>")
+def start_date(start=None):
+#   Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a given start or start-end range.
+#   When given the start only, calculate TMIN, TAVG, and TMAX for all dates greater than or equal to the start date.
+    #print(dt.datetime.strptime(start, "%m-%d-%Y").date())
+
+    session = Session(engine)
+    
+#   saving input as date to be used for filter in query
+    query_start_date = dt.datetime.strptime(start, "%m-%d-%Y")
+    
+#   querying for (in order) min, avg, and max temperature for all dates greater than or equal to the start date provided by the user
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= query_start_date).all()
+    #TMAX = session.query(func.max(Measurement.tobs)).filter(Measurement.date >= query_start_date).scalar()
+    #TAVG = session.query(func.avg(Measurement.tobs)).filter(Measurement.date >= query_start_date).scalar()
+
+    session.close()
+    
+#   converting results to list
+    date_query_results = list(np.ravel(results))
+
+#   returning JSON list of min, avg, max temp
+    return jsonify(date_query_results)
+
+@app.route("/api/v1.0/<start>/<end>")
+def start_end(start=None, end=None):
+#  When given the start and the end date, calculate the TMIN, TAVG, and TMAX for dates from the start date through the end date (inclusive).
+    
+    session = Session(engine)
+    
+#   saving inputs as start and end dates to be used for filtering query    
+    query_start_date = dt.datetime.strptime(start, "%m-%d-%Y")
+    query_end_date = dt.datetime.strptime(end, "%m-%d-%Y")
+
+#   querying for (in order) min, avg, and max temperature for all dates within the date range provided by the user (inclusive)
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).filter(Measurement.date >= query_start_date).filter(Measurement.date <= query_end_date).all()
+
+    session.close()
+
+#   converting results to list
+    date_query_results = list(np.ravel(results))
+
+#   returning JSON list of min, avg, max temp
+    return jsonify(date_query_results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
